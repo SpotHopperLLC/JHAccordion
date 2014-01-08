eb60791fdfd65a8bf300175c8182c643a9d3a271
Josh Holtz <josh@rokkincat.com>
2014-01-08 12:56:41 -0600

    My reviews uses dropdown filter and sort components

---
+++ SpotHopper/DropdownOptionCell.h
@@ -0,0 +1,15 @@
+//
+//  DropdownOptionCell.h
+//  SpotHopper
+//
+//  Created by Josh Holtz on 1/8/14.
+//  Copyright (c) 2014 RokkinCat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DropdownOptionCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *lblText;
+
+@end
--- SpotHopper/MyReviewsViewController.m
+++ SpotHopper/MyReviewsViewController.m
@@ -6,11 +6,17 @@
 //  Copyright (c) 2014 RokkinCat. All rights reserved.
 //
 
+#define kFilters @[@"All Reviews", @"Spots", @"Beers", @"Wines", @"Cocktails"]
+#define kFiltersIcons @[@"", @"", @"", @"", @""]
+#define kSorts @[@"Most Recent", @"Highest Rated"]
+#define kSortsIcons @[@"", @""]
+
 #import "MyReviewsViewController.h"
 
 #import "UIViewController+Navigator.h"
 
 #import "SectionHeaderView.h"
+#import "DropdownOptionCell.h"
 
 #import <JHAccordion/JHAccordion.h>
 
@@ -20,12 +26,13 @@
 @property (weak, nonatomic) IBOutlet UITableView *tblReviews;
 
 @property (nonatomic, strong) JHAccordion *accordion;
+@property (nonatomic, strong) SectionHeaderView *sectionHeaderFilter;
+@property (nonatomic, strong) SectionHeaderView *sectionHeaderSort;
 
-@property (nonatomic, strong) SectionHeaderView *sectionHeaderBeer;
-@property (nonatomic, strong) SectionHeaderView *sectionHeaderWine;
-@property (nonatomic, strong) SectionHeaderView *sectionHeaderCocktails;
-@property (nonatomic, strong) SectionHeaderView *sectionHeaderLiqour;
-@property (nonatomic, strong) SectionHeaderView *sectionHeaderMostRecent;
+@property (nonatomic, assign) NSInteger selectedFilter;
+@property (nonatomic, assign) NSInteger selectedSort;
+
+@property (nonatomic, strong) NSMutableArray *reviews;
 
 @end
 
@@ -56,6 +63,11 @@
     
     // Configures table
     [_tblReviews setTableFooterView:[[UIView alloc] init]];
+    [_tblReviews registerNib:[UINib nibWithNibName:@"DropdownOptionCellView" bundle:[NSBundle mainBundle]] forCellReuseIdentifier:@"DropdownOptionCell"];
+    
+    // Initializes states
+    _selectedFilter = 0;
+    _selectedSort = 0;
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -79,24 +91,67 @@
 #pragma mark - UITableViewDataSource
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 5;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return 3;
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if (section == 0) {
+        return kFilters.count;
+    } else if (section == 1) {
+        return kSorts.count;
+    } else if (section == 2) {
+        return 5;
+    }
+    return 0;
+}
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    if (indexPath.section == 0) {
+        NSString *text = [kFilters objectAtIndex:indexPath.row];
+        
+        DropdownOptionCell *cell = [tableView dequeueReusableCellWithIdentifier:@"DropdownOptionCell" forIndexPath:indexPath];
+        [cell.lblText setText:text];
+        
+        return cell;
+    } else if (indexPath.section == 1) {
+        NSString *text = [kSorts objectAtIndex:indexPath.row];
+        
+        DropdownOptionCell *cell = [tableView dequeueReusableCellWithIdentifier:@"DropdownOptionCell" forIndexPath:indexPath];
+        
+        [cell.lblText setText:text];
+        
+        return cell;
+
+    } else if (indexPath.section == 2) {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ReviewCell" forIndexPath:indexPath];
+        return cell;
+    }
     
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ReviewCell" forIndexPath:indexPath];
-    
-    return cell;
+    return nil;
 }
 
 #pragma mark - UITableViewDelegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    [self goToReview:nil];
+    if (indexPath.section == 0) {
+        _selectedFilter = indexPath.row;
+        
+        [self updateViewHeader:indexPath.section];
+        
+        [_accordion closeSection:indexPath.section];
+        [_tblReviews deselectRowAtIndexPath:indexPath animated:NO];
+    } else if (indexPath.section == 1) {
+        _selectedSort = indexPath.row;
+        
+        [self updateViewHeader:indexPath.section];
+        
+        [_accordion closeSection:indexPath.section];
+        [_tblReviews deselectRowAtIndexPath:indexPath animated:NO];
+    } else if (indexPath.section == 2) {
+        [self goToReview:nil];
+    }
+    
 }
 
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
@@ -104,99 +159,76 @@
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
-    return 56.0f;
+    if (section == 0 || section == 1) {
+        return 56.0f;
+    }
+    
+    return 0.0f;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     // This sets all rows in the closed sections to a height of 0 (so they won't be shown)
     // and the opened section to a height of 44.0
-    return ( [_accordion isSectionOpened:indexPath.section] ? 72.0f : 0.0f);
+    if (indexPath.section == 0 || indexPath.section == 1) {
+        return ( [_accordion isSectionOpened:indexPath.section] ? 44.0f : 0.0f);
+    } else if (indexPath.section == 2) {
+        return 72.0f;
+    }
+    
+    return 0.0f;
 }
 
 #pragma mark - JHAccordionDelegate
 
 - (void)accordionOpenedSection:(NSInteger)section {
-    if (section == 0) [_sectionHeaderBeer setSelected:YES];
-    if (section == 1) [_sectionHeaderWine setSelected:YES];
-    if (section == 2) [_sectionHeaderCocktails setSelected:YES];
-    if (section == 3) [_sectionHeaderLiqour setSelected:YES];
-    if (section == 4) [_sectionHeaderMostRecent setSelected:YES];
+    if (section == 0) [_sectionHeaderFilter setSelected:YES];
+    if (section == 1) [_sectionHeaderSort setSelected:YES];
 }
 
 - (void)accordionClosedSection:(NSInteger)section {
-    if (section == 0) [_sectionHeaderBeer setSelected:NO];
-    if (section == 1) [_sectionHeaderWine setSelected:NO];
-    if (section == 2) [_sectionHeaderCocktails setSelected:NO];
-    if (section == 3) [_sectionHeaderLiqour setSelected:NO];
-    if (section == 4) [_sectionHeaderMostRecent setSelected:NO];
+    if (section == 0) [_sectionHeaderFilter setSelected:NO];
+    if (section == 1) [_sectionHeaderSort setSelected:NO];
 }
 
 #pragma mark - Private
 
 - (SectionHeaderView*)sectionHeaderViewForSection:(NSInteger)section {
     if (section == 0) {
-        if (_sectionHeaderBeer == nil) {
-            _sectionHeaderBeer = [[SectionHeaderView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, CGRectGetWidth(_tblReviews.frame), 56.0f)];
-            [_sectionHeaderBeer setIconImage:[UIImage imageNamed:@"icon_beer"]];
-            [_sectionHeaderBeer setText:@"Beer"];
+        if (_sectionHeaderFilter == nil) {
+            _sectionHeaderFilter = [[SectionHeaderView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, CGRectGetWidth(_tblReviews.frame), 56.0f)];
             
             // Sets up for accordion
-            [_sectionHeaderBeer.btnBackground setTag:section];
-            [_sectionHeaderBeer.btnBackground addTarget:_accordion action:@selector(onClickSection:) forControlEvents:UIControlEventTouchUpInside];
+            [_sectionHeaderFilter.btnBackground setTag:section];
+            [_sectionHeaderFilter.btnBackground addTarget:_accordion action:@selector(onClickSection:) forControlEvents:UIControlEventTouchUpInside];
 
+            [self updateViewHeader:section];
         }
         
-        return _sectionHeaderBeer;
+        return _sectionHeaderFilter;
     } else if (section == 1) {
-        if (_sectionHeaderWine == nil) {
-            _sectionHeaderWine = [[SectionHeaderView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, CGRectGetWidth(_tblReviews.frame), 56.0f)];
-            [_sectionHeaderWine setIconImage:[UIImage imageNamed:@"icon_wine"]];
-            [_sectionHeaderWine setText:@"Wine"];
+        if (_sectionHeaderSort == nil) {
+            _sectionHeaderSort = [[SectionHeaderView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, CGRectGetWidth(_tblReviews.frame), 56.0f)];
 
             // Sets up for accordion
-            [_sectionHeaderWine.btnBackground setTag:section];
-            [_sectionHeaderWine.btnBackground addTarget:_accordion action:@selector(onClickSection:) forControlEvents:UIControlEventTouchUpInside];
+            [_sectionHeaderSort.btnBackground setTag:section];
+            [_sectionHeaderSort.btnBackground addTarget:_accordion action:@selector(onClickSection:) forControlEvents:UIControlEventTouchUpInside];
+            
+            [self updateViewHeader:section];
         }
         
-        return _sectionHeaderWine;
-    } else if (section == 2) {
-        if (_sectionHeaderCocktails == nil) {
-            _sectionHeaderCocktails = [[SectionHeaderView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, CGRectGetWidth(_tblReviews.frame), 56.0f)];
-            [_sectionHeaderCocktails setIconImage:[UIImage imageNamed:@"icon_cocktails"]];
-            [_sectionHeaderCocktails setText:@"Cocktail"];
-
-            // Sets up for accordion
-            [_sectionHeaderCocktails.btnBackground setTag:section];
-            [_sectionHeaderCocktails.btnBackground addTarget:_accordion action:@selector(onClickSection:) forControlEvents:UIControlEventTouchUpInside];
-        }
-        
-        return _sectionHeaderCocktails;
-    } else if (section == 3) {
-        if (_sectionHeaderLiqour == nil) {
-            _sectionHeaderLiqour = [[SectionHeaderView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, CGRectGetWidth(_tblReviews.frame), 56.0f)];
-            [_sectionHeaderLiqour setIconImage:[UIImage imageNamed:@"icon_liquor"]];
-            [_sectionHeaderLiqour setText:@"Liquor"];
-
-            // Sets up for accordion
-            [_sectionHeaderLiqour.btnBackground setTag:section];
-            [_sectionHeaderLiqour.btnBackground addTarget:_accordion action:@selector(onClickSection:) forControlEvents:UIControlEventTouchUpInside];
-        }
-        
-        return _sectionHeaderLiqour;
-    } else if (section == 4) {
-        if (_sectionHeaderMostRecent == nil) {
-            _sectionHeaderMostRecent = [[SectionHeaderView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, CGRectGetWidth(_tblReviews.frame), 56.0f)];
-            [_sectionHeaderMostRecent setIconImage:[UIImage imageNamed:@"icon_clock"]];
-            [_sectionHeaderMostRecent setText:@"Most Recent"];
-
-            // Sets up for accordion
-            [_sectionHeaderMostRecent.btnBackground setTag:section];
-            [_sectionHeaderMostRecent.btnBackground addTarget:_accordion action:@selector(onClickSection:) forControlEvents:UIControlEventTouchUpInside];
-        }
-        
-        return _sectionHeaderMostRecent;
+        return _sectionHeaderSort;
     }
     return nil;
+}
+
+- (void)updateViewHeader:(NSInteger)section {
+    if (section == 0) {
+        [_sectionHeaderFilter setIconImage:[UIImage imageNamed:[kFiltersIcons objectAtIndex:_selectedFilter]]];
+        [_sectionHeaderFilter setText:[kFilters objectAtIndex:_selectedFilter]];
+    } else if (section == 1) {
+        [_sectionHeaderSort setIconImage:[UIImage imageNamed:[kSortsIcons objectAtIndex:_selectedSort]]];
+        [_sectionHeaderSort setText:[kSorts objectAtIndex:_selectedSort]];
+    }
 }
 
 @end
