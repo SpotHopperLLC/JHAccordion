aba8d17619aa757c323a5a1a0b14f7219c6889ee
Josh Holtz <josh@rokkincat.com>
2014-02-19 16:45:33 -0600

    Created button that is specifically for location finding and storing

---
+++ SpotHopper/SHButtonLatoLightLocation.h
@@ -0,0 +1,13 @@
+//
+//  SHButtonLatoLightLocation.h
+//  SpotHopper
+//
+//  Created by Josh Holtz on 2/19/14.
+//  Copyright (c) 2014 RokkinCat. All rights reserved.
+//
+
+#import "SHButtonLatoLight.h"
+
+@interface SHButtonLatoLightLocation : SHButtonLatoLight
+
+@end
---
+++ SpotHopper/SHButtonLatoLightLocation.m
@@ -0,0 +1,31 @@
+//
+//  SHButtonLatoLightLocation.m
+//  SpotHopper
+//
+//  Created by Josh Holtz on 2/19/14.
+//  Copyright (c) 2014 RokkinCat. All rights reserved.
+//
+
+#import "SHButtonLatoLightLocation.h"
+
+@implementation SHButtonLatoLightLocation
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
