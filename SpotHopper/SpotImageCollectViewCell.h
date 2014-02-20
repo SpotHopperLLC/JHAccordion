d01a644cb42c42681577e9baaa5b4adf8b740efd
Josh Holtz <josh@rokkincat.com>
2014-02-20 11:38:14 -0600

    We got image scroller for spot profile

---
+++ SpotHopper/SpotImageCollectViewCell.h
@@ -0,0 +1,15 @@
+//
+//  SpotImageCollectViewCell.h
+//  SpotHopper
+//
+//  Created by Josh Holtz on 2/20/14.
+//  Copyright (c) 2014 RokkinCat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpotImageCollectViewCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *imgSpot;
+
+@end
--- SpotHopper/SpotProfileViewController.m
+++ SpotHopper/SpotProfileViewController.m
@@ -10,11 +10,22 @@
 
 #import "UIView+ViewFromNib.h"
 
+#import "SpotImageCollectViewCell.h"
+
+#import <AFNetworking/UIImageView+AFNetworking.h>
+
 @interface SpotProfileViewController ()<UITableViewDataSource, UITableViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tblSliders;
 
+// Static Header
+@property (weak, nonatomic) IBOutlet UILabel *lblSpotType;
+
+// Header
 @property (nonatomic, strong) UIView *headerContent;
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (weak, nonatomic) IBOutlet UIButton *btnImagePrev;
+@property (weak, nonatomic) IBOutlet UIButton *btnImageNext;
 
 @end
 
@@ -43,11 +54,22 @@
     // Header content view
     _headerContent = [UIView viewFromNibNamed:@"SpotProfileHeaderView" withOwner:self];
     [_tblSliders setTableHeaderView:_headerContent];
+
+    // Custom collection view layout
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+    [layout setScrollDirection:UICollectionViewScrollDirectionHorizontal];
+    [layout setItemSize:CGSizeMake(320.0f, 165.0f)];
+    [layout setMinimumInteritemSpacing:0.0f];
+    [layout setMinimumLineSpacing:0.0f];
+    [_collectionView setCollectionViewLayout:layout];
+    [_collectionView setPagingEnabled:YES];
+    
+    // Configure collection cell
+    [_collectionView registerNib:[UINib nibWithNibName:@"SpotImageCollectionViewCellView" bundle:[NSBundle mainBundle]] forCellWithReuseIdentifier:@"SpotImageCollectViewCell"];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    [self.navigationController setNavigationBarHidden:YES animated:animated];
     
     // Adds contextual footer view
     [self addFooterViewController:^(FooterViewController *footerViewController) {
@@ -62,6 +84,24 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UICollectionViewDataSource
+
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 1;
+}
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 3;
+}
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    SpotImageCollectViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"SpotImageCollectViewCell" forIndexPath:indexPath];
+    [cell.imgSpot setImageWithURL:[NSURL URLWithString:@"http://placekitten.com/320/165"]];
+    
+    return cell;
 }
 
 #pragma mark - UITableViewDataSource
@@ -91,6 +131,34 @@
 #pragma mark - UITableViewDelegate
 
 #pragma mark - Actions
+
+- (IBAction)onClickImagePrevious:(id)sender {
+    NSArray *indexPaths = [_collectionView indexPathsForVisibleItems];
+    
+    // Makes sure we have an index path
+    if (indexPaths.count > 0) {
+        
+        NSIndexPath *indexPath = [indexPaths objectAtIndex:0];
+        // Makes sure we can go back
+        if (indexPath.row > 0) {
+            [_collectionView scrollToItemAtIndexPath:[NSIndexPath indexPathForRow:(indexPath.row - 1) inSection:indexPath.section] atScrollPosition:UICollectionViewScrollPositionCenteredHorizontally animated:YES];
+        }
+    }
+}
+
+- (IBAction)onClickImageNext:(id)sender {
+    NSArray *indexPaths = [_collectionView indexPathsForVisibleItems];
+    
+    // Makes sure we have an index path
+    if (indexPaths.count > 0) {
+        
+        NSIndexPath *indexPath = [indexPaths objectAtIndex:0];
+        // Makes sure we can go forward
+        if (indexPath.row < ( [self collectionView:_collectionView numberOfItemsInSection:indexPath.section] - 1) ) {
+            [_collectionView scrollToItemAtIndexPath:[NSIndexPath indexPathForRow:(indexPath.row + 1) inSection:indexPath.section] atScrollPosition:UICollectionViewScrollPositionCenteredHorizontally animated:YES];
+        }
+    }
+}
 
 #pragma mark - Private
 
