8bcc332a19bc3da11a501d648577b9b8ee7ac6bd
Josh Holtz <josh@rokkincat.com>
2014-03-28 09:42:01 -0500

    Rebasing is so much fun

---
+++ SpotHopper/ShareViewController.m
@@ -0,0 +1,38 @@
+//
+//  ShareViewController.m
+//  SpotHopper
+//
+//  Created by Josh Holtz on 3/28/14.
+//  Copyright (c) 2014 RokkinCat. All rights reserved.
+//
+
+#import "ShareViewController.h"
+
+@interface ShareViewController ()
+
+@end
+
+@implementation ShareViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
