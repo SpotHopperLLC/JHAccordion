e8c827903606974d5c7d35afb07fab9f4b338e6c
Tracee Pettigrew <pettigree@yahoo.com>
2014-06-03 13:20:51 -0500

    fixed auto layout issues

--- SpotHopper/SHSpotProfileViewController.m
+++ SpotHopper/SHSpotProfileViewController.m
@@ -11,6 +11,10 @@
 #import "SpotModel.h"
 #import "LiveSpecialModel.h"
 #import "SpotTypeModel.h"
+#import "SliderTemplateModel.h"
+
+#import "SHSlider.h"
+
 #import "PhotoAlbumViewController.h"
 #import "PhotoViewerViewController.h"
 
@@ -32,14 +36,22 @@
 #define kLabelTagSpotSpecial 1
 #define kLabelTagSpotSpecialDetails 2
 
+#define kLeftLabelVibeTag 1
+#define kRightLabelVibeTag 2
+#define kSliderVibeTag 3
+
+
 #define kCollectionViewTag 1
 #define kPreviousBtnTag 2
 #define kNextBtnTag 3
+
+
 
 #define kNumberOfCells 3
 
 NSString* const DrinkProfileToPhotoViewer = @"DrinkProfileToPhotoViewer";
 NSString* const DrinkProfileToPhotoAlbum = @"DrinkProfileToPhotoAlbum";
+
 
 @interface SHSpotProfileViewController () <UITableViewDataSource, UITableViewDelegate, SHImageModelCollectionDelegate>
 
@@ -59,10 +71,21 @@ NSString* const DrinkProfileToPhotoAlbum = @"DrinkProfileToPhotoAlbum";
 #pragma mark -
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
+    return 2;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    switch (section) {
+        case 0:
+            return kNumberOfCells;
+            break;
+        case 1:
+            return [self.spot.sliderTemplates count];
+            break;
+        default:
+            break;
+    }
+    
     return kNumberOfCells; // todo: + # of slider cells needed
 }
 
@@ -70,97 +93,109 @@ NSString* const DrinkProfileToPhotoAlbum = @"DrinkProfileToPhotoAlbum";
     static NSString *CollectionViewCellIdentifier = @"CollectionViewCell";
     static NSString *SpotDetailsCellIdentifier = @"SpotDetailsCell";
     static NSString *SpotSpecialsCellIdentifier = @"SpotSpecialsCell";
+    static NSString *SpotVibeIdentifier = @"SpotVibeCell";
+
     
     UITableViewCell *cell;
     
-    switch (indexPath.row) {
-        case kCellImageCollection: {
-        
-                cell = [tableView dequeueReusableCellWithIdentifier:CollectionViewCellIdentifier];
-                //todo: place collection view here
-                
-                UICollectionView *collectionView = (UICollectionView *)[cell viewWithTag:kCollectionViewTag];
-            
-                self.imageModelCollectionViewManager.collectionView = collectionView;
-                collectionView.delegate = self.imageModelCollectionViewManager;
-                collectionView.dataSource = self.imageModelCollectionViewManager;
-                self.imageModelCollectionViewManager.imageModels = self.spot.images;
-            
-                //attach previous and next buttons to goPrevious and goNext to trigger image transitions
-                UIButton *previousButton = (UIButton *)[cell viewWithTag:kPreviousBtnTag];
-                [previousButton addTarget:self.imageModelCollectionViewManager action:@selector(goPrevious) forControlEvents:UIControlEventTouchUpInside];
-            
-                UIButton *nextButton = (UIButton *)[cell viewWithTag:kNextBtnTag];
-                [nextButton addTarget:self.imageModelCollectionViewManager action:@selector(goNext) forControlEvents:UIControlEventTouchUpInside];
-            
-                [self didReachEnd:[self.imageModelCollectionViewManager hasPrevious] button:previousButton];
-                [self didReachEnd:[self.imageModelCollectionViewManager hasNext] button:nextButton];
-            
-                break;
-            }
+    switch (indexPath.section) {
+        case 0:{
+            switch (indexPath.row) {
+                case kCellImageCollection: {
+                    
+                    cell = [tableView dequeueReusableCellWithIdentifier:CollectionViewCellIdentifier];
+                    //todo: place collection view here
+                    
+                    UICollectionView *collectionView = (UICollectionView *)[cell viewWithTag:kCollectionViewTag];
+                    
+                    self.imageModelCollectionViewManager.collectionView = collectionView;
+                    collectionView.delegate = self.imageModelCollectionViewManager;
+                    collectionView.dataSource = self.imageModelCollectionViewManager;
+                    self.imageModelCollectionViewManager.imageModels = self.spot.images;
+                    
+                    //attach previous and next buttons to goPrevious and goNext to trigger image transitions
+                    UIButton *previousButton = (UIButton *)[cell viewWithTag:kPreviousBtnTag];
+                    [previousButton addTarget:self.imageModelCollectionViewManager action:@selector(goPrevious) forControlEvents:UIControlEventTouchUpInside];
+                    
+                    UIButton *nextButton = (UIButton *)[cell viewWithTag:kNextBtnTag];
+                    [nextButton addTarget:self.imageModelCollectionViewManager action:@selector(goNext) forControlEvents:UIControlEventTouchUpInside];
+                    
+                    [self didReachEnd:[self.imageModelCollectionViewManager hasPrevious] button:previousButton];
+                    [self didReachEnd:[self.imageModelCollectionViewManager hasNext] button:nextButton];
+                    
+                    break;
+                }
+                    
+                case kCellSpotDetails:{
+                    
+                    //todo: add defensive programming for checking whether labels exist?
+                    cell = [tableView dequeueReusableCellWithIdentifier:SpotDetailsCellIdentifier];
+                    
+                    UILabel *spotName = (UILabel*)[cell viewWithTag:kLabelTagSpotName];
+                    [SHStyleKit setLabel:spotName textColor:SHStyleKitColorMyTintColor];
+                    spotName.text = self.spot.name;
+                    
+                    //todo:update to display the spot type as well as the expense
+                    UILabel *spotType = (UILabel*)[cell viewWithTag:kLabelTagSpotType];
+                    spotType.text = self.spot.spotType.name;
+                    
+                    UILabel *spotRelevancy = (UILabel*)[cell viewWithTag:kLabelTagSpotRelevancy];
+                    spotRelevancy.text = [NSString stringWithFormat:@"%@ Match",self.spot.matchPercent];
+                    
+                    UILabel *spotCloseTime = (UILabel*)[cell viewWithTag:kLabelTagSpotCloseTime];
+                    NSString *closeTime;
+                    
+                    if (!(closeTime = [self findCloseTimeForToday])) {
+                        spotCloseTime.text = closeTime;
+                    }
+                    
+                    UILabel *spotAddress = (UILabel*)[cell viewWithTag:kLabelTagSpotAddress];
+                    spotAddress.text = self.spot.addressCityState;
+                    break;
+                }
+                    
+                case kCellSpotSpecials:{
+                    
+                    cell = [tableView dequeueReusableCellWithIdentifier:SpotSpecialsCellIdentifier];
 
-        case kCellSpotDetails:{
-            
-            cell = [tableView dequeueReusableCellWithIdentifier:SpotDetailsCellIdentifier];
-            
-            UILabel *spotName = (UILabel*)[cell viewWithTag:kLabelTagSpotName];
-            [SHStyleKit setLabel:spotName textColor:SHStyleKitColorMyTintColor];
-            spotName.text = self.spot.name;
-            
-            //todo:update to display the spot type as well as the expense
-            UILabel *spotType = (UILabel*)[cell viewWithTag:kLabelTagSpotType];
-            spotType.text = self.spot.spotType.name;
-            
-            UILabel *spotRelevancy = (UILabel*)[cell viewWithTag:kLabelTagSpotRelevancy];
-            spotRelevancy.text = [NSString stringWithFormat:@"%@ Match",self.spot.matchPercent];
-            
-            UILabel *spotCloseTime = (UILabel*)[cell viewWithTag:kLabelTagSpotCloseTime];
-            NSString *closeTime;
-            if (!(closeTime = [self findCloseTimeForToday])) {
-                spotCloseTime.text = closeTime;
+                    NSArray *dailySpecials;
+                    
+                    if ((dailySpecials = self.spot.dailySpecials)) {
+                        //todo: ask if this is needed
+                        //UILabel *spotSpecial = (UILabel*)[cell viewWithTag:kLabelTagSpotSpecial];
+                        UILabel *specialDetails = (UILabel*)[cell viewWithTag:kLabelTagSpotSpecialDetails];
+                        
+                        NSString *todaysSpecial = [self.spot.dailySpecials specialsForToday];
+                        
+                        if (todaysSpecial) {
+                            specialDetails.text = todaysSpecial;
+                        }
+                        
+                    }
+                    break;
+                }
             }
             
-            UILabel *spotAddress = (UILabel*)[cell viewWithTag:kLabelTagSpotAddress];
-            spotAddress.text = self.spot.addressCityState;
             break;
         }
-        
-        case kCellSpotSpecials:{
+        case 1:{
+            cell = [tableView dequeueReusableCellWithIdentifier:SpotVibeIdentifier];
+            SHSlider *slider = (SHSlider*)[cell viewWithTag:kSliderVibeTag];
+            UILabel *minValue = (UILabel*)[cell viewWithTag:kLeftLabelVibeTag];
+            UILabel *maxValue = (UILabel*)[cell viewWithTag:kRightLabelVibeTag];
             
-            cell = [tableView dequeueReusableCellWithIdentifier:SpotSpecialsCellIdentifier];
+            SliderTemplateModel *sliderTemplate = self.spot.sliderTemplates[indexPath.row];
+            minValue.text = sliderTemplate.minLabel;
+            maxValue.text = sliderTemplate.maxLabel;
+            [slider setSelectedValue:[sliderTemplate.defaultValue floatValue]];
             
-            NSArray *dailySpecials;
-
-            if ((dailySpecials = self.spot.dailySpecials)) {
-                UILabel *spotSpecial = (UILabel*)[cell viewWithTag:kLabelTagSpotSpecial];
-                UILabel *specialDetails = (UILabel*)[cell viewWithTag:kLabelTagSpotSpecialDetails];
-
-                NSString *todaysSpecial = [self.spot.dailySpecials specialsForToday];
-                
-                if (todaysSpecial) {
-                    specialDetails.text = todaysSpecial;
-                }
-                
-//                LiveSpecialModel *liveSpecial = [self.spot currentLiveSpecial];
-//                NSString *todaysSpecial = [dailySpecials specialsForToday];
-//
-//                if (liveSpecial) {
-//                    spotSpecial.text = @"Live Special!";
-//                    specialDetails.text = liveSpecial.text;
-//        
-//                } else if (todaysSpecial) {
-//                    spotSpecial.text = @"Current Special!";
-//                    specialDetails.text = todaysSpecial;
-//                }
-            }
-    
             break;
         }
         default:
-            //todo: place switches here (add logic for creating as many switches as vibe dictates)
             break;
     }
-    return cell;
+    
+        return cell;
 }
 
 #pragma mark - UITableViewDelegate
@@ -171,28 +206,38 @@ NSString* const DrinkProfileToPhotoAlbum = @"DrinkProfileToPhotoAlbum";
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    CGFloat height;
+    CGFloat height = 0.0f;
     
     NSString *todaysSpecial = [self.spot.dailySpecials specialsForToday];
     
-    switch (indexPath.row) {
-        case kCellImageCollection:
+    switch (indexPath.section) {
+        case 1:{
+            switch (indexPath.row) {
+                case kCellImageCollection:
+                    height = 178.0f;
+                    //todo: check with Brennan
+                    break;
+                case kCellSpotDetails:
+                    height = 131.0f;
+                    //todo: check with Brennan
+                    break;
+                case kCellSpotSpecials:
+                    height = todaysSpecial.length ? 91.0f : 0.0f;
+                    break;
+                default:
+                    break;
+            }
+            break;
+        }
+        case 2:{
             height = 178.0f;
-            //todo: check with Brennan
             break;
-        case kCellSpotDetails:
-            height = 131.0f;
-            //todo: check with Brennan
-            break;
-        case kCellSpotSpecials:
-            height = todaysSpecial.length ? 91.0f : 0.0f;
-            break;
+        }
         default:
-            //todo: figure out height of the slider cells
-            height = 0.0f;
             break;
     }
-    return height;
+  
+    return height; 
 }
 
 
@@ -228,9 +273,9 @@ NSString* const DrinkProfileToPhotoAlbum = @"DrinkProfileToPhotoAlbum";
     // Sets "Opens at <some time>" or "Open until <some time>"
     NSString *closeTime = @"";
     NSArray *hoursForToday = [self.spot.hoursOfOperation datesForToday];
-    NSLog(@"spot hours: %@", self.spot.hoursOfOperation);
     
-    if (!hoursForToday) {
+    
+    if (hoursForToday) {
     
         // Creats formatter
         NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
