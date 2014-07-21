ad95ea5828cdc9abbaee0497dc9122cc712f7617
Brennan Stehling <brennan@smallsharptools.com>
2014-07-21 17:19:23 -0500

    fix for scoping issue #852

--- SpotHopper/SHHomeMapViewController.m
+++ SpotHopper/SHHomeMapViewController.m
@@ -1584,6 +1584,23 @@ NSString* const HomeMapToDrinkProfile = @"HomeMapToDrinkProfile";
     return visibleRegion.center;
 }
 
+- (CLLocationCoordinate2D)adjustedCenterCoordinate {
+    // the target coordinate is directly in the center horizontally
+    // and just above the bottom of the status view by half of the
+    // annotation view and a margin value of about 10 points.
+    
+    // the key is that the zoom level is not changing
+    
+    // weighted center is south of the visible center
+    
+    // 1) get the UIView position for the target position
+    // 2) translate the target position to map coordinates
+    // 3) calculate the latitude delta between center coord and target coord
+    // 4) calcualate new center coord using new point
+    
+    return kCLLocationCoordinate2DInvalid;
+}
+
 - (CLLocationDistance)searchRadius {
     MKCoordinateRegion visibleRegion = [self visibleMapRegion];
     CLLocationCoordinate2D visibleCenter = visibleRegion.center;
@@ -2572,7 +2589,7 @@ NSString* const HomeMapToDrinkProfile = @"HomeMapToDrinkProfile";
     
     [self updateLocationName];
     
-    if (!self.scopedSpot && [self canSearchAgain]) {
+    if ([self canSearchAgain]) {
         [self showSearchThisArea:TRUE withCompletionBlock:nil];
     }
     
@@ -2708,6 +2725,10 @@ NSString* const HomeMapToDrinkProfile = @"HomeMapToDrinkProfile";
             request.drinkId = drinkModel.ID;
             request.drinkTypeId = drinkModel.drinkType.ID;
             request.drinkSubTypeId = drinkModel.drinkSubtype.ID;
+            
+            if (self.isScopedToSpot) {
+                request.spotId = self.scopedSpot.ID;
+            }
             
             SHMode mode = [self modeForDrink:drinkModel];
             
