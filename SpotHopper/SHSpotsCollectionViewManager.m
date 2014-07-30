9e17bd716a8f5940ecdcc43b94eaf4a3fb05e6ba
Brennan Stehling <brennan@smallsharptools.com>
2014-07-30 17:32:05 -0500

    left arrow is not hidden when it should be #894

--- SpotHopper/SHSpotsCollectionViewManager.m
+++ SpotHopper/SHSpotsCollectionViewManager.m
@@ -295,12 +295,6 @@
     positionLabel.text = [NSString stringWithFormat:@"%lu of %lu", (long)index+1, (long)self.spotList.spots.count];
     percentageLabel.text = [NSString stringWithFormat:@"%@", spot.matchPercent];
     
-    [SHStyleKit setButton:previousButton withDrawing:SHStyleKitDrawingArrowLeftIcon normalColor:SHStyleKitColorMyTextColor highlightedColor:SHStyleKitColorMyWhiteColor];
-    previousButton.hidden = index == 0;
-    
-    [SHStyleKit setButton:nextButton withDrawing:SHStyleKitDrawingArrowRightIcon normalColor:SHStyleKitColorMyTextColor highlightedColor:SHStyleKitColorMyWhiteColor];
-    nextButton.hidden = index == self.spotList.spots.count - 1;
-    
     if (self.spotList.spots.count == 1) {
         matchImageView.image = nil;
         
@@ -319,12 +313,16 @@
 
         percentageLabel.hidden = FALSE;
         matchLabel.hidden = FALSE;
-        previousButton.hidden = FALSE;
-        nextButton.hidden = FALSE;
         positionLabel.hidden = FALSE;
         findSimilarButton.hidden = TRUE;
         reviewItButton.hidden = TRUE;
         menuButton.hidden = TRUE;
+        
+        [SHStyleKit setButton:previousButton withDrawing:SHStyleKitDrawingArrowLeftIcon normalColor:SHStyleKitColorMyTextColor highlightedColor:SHStyleKitColorMyWhiteColor];
+        previousButton.hidden = index == 0;
+        
+        [SHStyleKit setButton:nextButton withDrawing:SHStyleKitDrawingArrowRightIcon normalColor:SHStyleKitColorMyTextColor highlightedColor:SHStyleKitColorMyWhiteColor];
+        nextButton.hidden = index == self.spotList.spots.count - 1;
     }
 }
 
